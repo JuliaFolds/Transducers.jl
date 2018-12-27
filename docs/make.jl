890b2a3af9886fa5ee58c01c65c54531a89b4fd1
Takafumi Arakaki <aka.tkf@gmail.com>
2018-12-26 16:00:05 -0800

    Build documentation in Travis

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,15 @@
+#!/bin/bash
+# -*- mode: julia -*-
+#=
+JULIA="${JULIA:-julia --color=yes --startup-file=no}"
+export JULIA_PROJECT="$(dirname ${BASH_SOURCE[0]})"
+
+set -ex
+${JULIA} -e 'using Pkg; Pkg.instantiate();
+             Pkg.develop(PackageSpec(path=pwd()))'
+exec ${JULIA} "${BASH_SOURCE[0]}" "$@"
+=#
+
 using Documenter, Transducers
 
 makedocs(;
