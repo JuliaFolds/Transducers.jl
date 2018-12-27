a994958e58ce56790931e16326501afb3c319c6f
Takafumi Arakaki <aka.tkf@gmail.com>
2018-12-26 16:06:14 -0800

    Fix subprocess path in test/test_ir.jl

--- test/test_ir.jl
+++ test/test_ir.jl
@@ -5,8 +5,10 @@ using Test
 if Base.JLOptions().check_bounds == 1
     # Run IR tests in a different process as `--check-bounds=yes` is
     # specified.
+    path = joinpath(@__DIR__, "__test_ir.jl")
     code = """
     $(Base.load_path_setup_code())
+    include($(repr(path)))
     """
     cmd = Base.julia_cmd()
     cmd = `$cmd --check-bounds=no`
