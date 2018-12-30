e43ceee905f3ddc2bf357bfdb65002e61e799cca
Takafumi Arakaki <aka.tkf@gmail.com>
2018-12-30 14:56:08 -0800

    Add bench_words.jl

---
+++ benchmark/bench_words.jl
@@ -0,0 +1,26 @@
+module BenchWords
+using BenchmarkTools
+include("../examples/words.jl")
+
+suite = BenchmarkGroup()
+
+function makewords(n)
+    genpos() = 2 ^ rand(0:5) + rand(0:10)
+    io = IOBuffer()
+    for _ in 1:n
+        for _ in 1:genpos()
+            write(io, ' ')
+        end
+        write(io, String(rand('a':'z', genpos())))
+    end
+    return String(take!(io))
+end
+
+str = makewords(2^14)
+
+for n in [1, 2, 4]
+    suite["nthreads=$n"] = @benchmarkable countwords($str; nthreads=$n)
+end
+
+end  # module
+BenchWords.suite
