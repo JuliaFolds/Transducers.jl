0dcc0c764996bd7f6f158b8140dabe1cb6cd4d6c
Takafumi Arakaki <aka.tkf@gmail.com>
2019-01-02 18:23:37 -0800

    Set --code-coverage=none in IR test

--- test/test_ir.jl
+++ test/test_ir.jl
@@ -2,16 +2,16 @@ module TestIR
 
 using Test
 
-if Base.JLOptions().check_bounds == 1
-    # Run IR tests in a different process as `--check-bounds=yes` is
-    # specified.
+if Base.JLOptions().check_bounds == 1 || Base.JLOptions().code_coverage != 0
+    # Run IR tests in a different process as `--check-bounds=yes` or
+    # --code-coverage={user|all} is specified.
     path = joinpath(@__DIR__, "__test_ir.jl")
     code = """
     $(Base.load_path_setup_code())
     include($(repr(path)))
     """
     cmd = Base.julia_cmd()
-    cmd = `$cmd --check-bounds=no`
+    cmd = `$cmd --check-bounds=no --code-coverage=none`
     if Base.JLOptions().color == 1
         cmd = `$cmd --color=yes`
     end
