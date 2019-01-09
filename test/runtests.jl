d9991f2fabbc305ebc4642692c6c7b4ea0e4c615
Takafumi Arakaki <aka.tkf@gmail.com>
2019-01-09 00:37:32 -0800

    Don't run tutorial_missings.jl with Julia 1.0

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,20 @@
 module TestTransducers
 using Test
 
-@testset "$file" for file in [
-        "test_library.jl",
-        "test_processes.jl",
-        "test_show.jl",
-        "test_examples_transducers.jl",
-        "test_examples_words.jl",
-        "test_examples_primes.jl",
-        "test_examples_tutorial_missings.jl",
-        "test_ir.jl",
-        ]
+testfiles = [
+    "test_library.jl",
+    "test_processes.jl",
+    "test_show.jl",
+    "test_examples_transducers.jl",
+    "test_examples_words.jl",
+    "test_examples_primes.jl",
+]
+if VERSION >= v"1.1-"
+    push!(testfiles, "test_examples_tutorial_missings.jl")
+end
+push!(testfiles, "test_ir.jl")
+
+@testset "$file" for file in testfiles
     include(file)
 end
 
