cebfc733d53bf805dee12071ac564ceb1c065e4c
Takafumi Arakaki <aka.tkf@gmail.com>
2019-01-16 19:26:49 -0800

    More texts in tutorial_missings.jl

--- examples/tutorial_missings.jl
+++ examples/tutorial_missings.jl
@@ -13,7 +13,7 @@ using Transducers
 mapfoldl(MapSplat(*), +, zip(1:3, 10:2:14))
 
 # Let's see what it does step by step.  First we create a "printer"
-# transducer using this function (see [`Map`](@ref)):
+# transducer using the following function (see [`Map`](@ref)):
 
 xf_printer(label) = Map() do x
     println(label, ": ", x)
@@ -56,7 +56,7 @@ mapfoldl(xf_mdot, +, zip(xs, ys))
 
 # ## Covariance
 #
-# This transducer `xf_mdot` can also be used to compute the
+# Transducer `xf_mdot` above can also be used to compute the
 # covariance.  First, we need the number of pairs of elements in `xs`
 # and `ys` that _both_ of them are not `missing`:
 
@@ -95,10 +95,11 @@ mapfoldl(xf_demean(xs, ys) |> xf_mdot, +, zip(xs, ys)) / nonmissings
 #
 # How do we use transducers for vector-to-vector transformation?  Here
 # is a function to calculate ``y = x + y`` while ignoring missing
-# values in ``x``.
+# values in ``x``.  First, mandatory input shape check:
 
 function add_skipmissing!(ys, xs)
     length(ys) == length(xs) || error("length(ys) != length(xs)")
+    firstindex(ys) == 1 || error("firstindex(ys) != 1")
 
 # For filtering out missing values from `xs` while tracking indices,
 # we use [`Zip`](@ref).  To iterate over the output of the transducer,
@@ -133,6 +134,7 @@ add_skipmissing!([100, 110, 120], [1, missing, 2])
 # ## Vectorized reduction
 #
 # `foldl`, `mapfoldl`, etc. in `Base` support `dims` argument.
+# Transducers.jl does not support this argument as of writing.
 # However, this can easily be emulated using `eachcol`, `eachrow`, or
 # `eachslice` iterators in `Base`.
 
@@ -206,8 +208,8 @@ end  # if VERSION >= v"1.1-"  #src
 # ## Argmax
 #
 # Another useful operation to do ignoring missing values is
-# `argmax`/`argmin`.  It can be implemented as `Zip(Count(), ...)`
-# (see also `add_skipmissing!` above) composed with
+# `argmax`/`argmin`.  It can be implemented using `Zip(Count(),
+# NotA(Missing))` (see also `add_skipmissing!` above) composed with
 # [`ScanEmit`](@ref):
 
 xf_argmax(xf_filter = NotA(Missing)) =
@@ -257,7 +259,8 @@ mapfoldl(xf_argmax(), right, [1, 3, missing, 2])
 
 # Side note: We use `typemin(Int)` as the initial value of `max` for
 # simplicity.  In practice, it should be
-# `typemin(eltype(input_array))`.
+# `typemin(eltype(input_array))`.  See the next section for how to do
+# it using `Initializer`.
 
 # We can use filter other than `NotA(Missing)`.  For example, to find
 # the index of the largest odd value:
@@ -265,18 +268,43 @@ mapfoldl(xf_argmax(), right, [1, 3, missing, 2])
 mapfoldl(xf_argmax(Filter(isodd)), right, [1, 4, 3, 2])
 
 # ## Extrema
+#
+# Transducer `xf_argmax` in the previous section only outputs the
+# index of the maximum element so far.  To output the maximum element
+# as well, we can simply use [`Scan`](@ref).  Also, while we are at
+# it, let's support both argmax and argmin.  To this end, we
+# parametrize the function passed to `Scan` by the comparison function
+# `>` and `<`.  Following function `argext_step` takes the function
+# `>` or `<` and return a function appropriate for `Scan.`
 
 argext_step(should_update) =
-    ((argext, ext), (index, value)) ->
-        should_update(ext, value) ? (index, value) : (argext, ext)
-
-argext_init(::typeof(>), ::Type{Tuple{F, S}}) where {F, S} = (zero(F), typemax(S))
-argext_init(::typeof(<), ::Type{Tuple{F, S}}) where {F, S} = (zero(F), typemin(S))
-
-xf_scanext(should_update) =
-    Scan(argext_step(should_update),
-         Initializer(TT -> argext_init(should_update, TT)))
+    ((oldindex, oldvalue), (index, value)) ->
+        should_update(oldvalue, value) ? (index, value) : (oldindex, oldvalue)
 nothing  # hide
+
+# Another problem with `xf_argmax` is that it does not handle
+# non-`Int` input types.  To properly handle different input types, we
+# use [`Initializer`](@ref).  `Initializer` needs a function that maps
+# an input _type_ to the initial state.  We first define a helper
+# function
+
+init_helper(::typeof(>), ::Type{Tuple{F, S}}) where {F, S} = (zero(F), typemax(S))
+init_helper(::typeof(<), ::Type{Tuple{F, S}}) where {F, S} = (zero(F), typemin(S))
+nothing  # hide
+
+# ...which is partially evaluated before passed to `Initializer`:
+
+argext_init(should_update) = Initializer(TT -> init_helper(should_update, TT))
+nothing  # hide
+
+# Finally we construct a `Scan` transducer using `argext_step` and
+# `argext_init`:
+
+xf_scanext(should_update) = Scan(argext_step(should_update),
+                                 argext_init(should_update))
+nothing  # hide
+
+# Passing `<` gives us the argmax transducer:
 
 @time begin  #src
 ans = # hide
@@ -285,6 +313,15 @@ end  #src
 #-
 @assert ans === (2, 3.0) # hide
 @show ans  #src
+
+# !!! warning
+#
+#     As of writing, following code takes a very long time (a few
+#     minutes) to compile.
+
+# We now have transducers `xf_scanext(<)` and `xf_scanext(>)` for
+# argmax and argmin, respectively.  We can compute them concurrently
+# by `Zip`'ing them together:
 
 xf_fullextrema(xf_filter = NotA(Missing)) =
     Zip(Count(), xf_filter) |> Zip(xf_scanext(>), xf_scanext(<))
@@ -296,6 +333,9 @@ end  #src
 #-
 @assert ans === ((3, -1.0), (2, 3.0))  # hide
 @show ans  #src
+
+# This transducer produces a tuple `((argmin, min), (argmax, max))`.
+# To output only indices, append an appropriate `Map`:
 
 xf_argextrema(xf_filter = NotA(Missing)) =
     xf_fullextrema(xf_filter) |> Map() do ((argmin, min), (argmax, max))
