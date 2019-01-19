5e2b58153e860cb7eaca0d79d3fce94d74cb932b
Takafumi Arakaki <aka.tkf@gmail.com>
2019-01-19 01:59:18 -0800

    Add bench_dot.jl

---
+++ benchmark/bench_dot.jl
@@ -0,0 +1,35 @@
+using BenchmarkTools
+using LinearAlgebra
+using Transducers
+
+function manual_dot(xs, ys)
+    acc = zero(promote_type(eltype(xs), eltype(ys)))
+    @inbounds @simd for i in eachindex(xs, ys)
+        acc += xs[i] * ys[i]
+    end
+    return acc
+end
+
+multiplied(iters...) = eduction(MapSplat(*), zip(iters...))
+
+let suite = BenchmarkGroup()
+
+    let xs = randn(10),
+        ys = zero(xs)
+        @assert manual_dot(xs, ys) == foldl(+, multiplied(xs, ys); simd=true)
+    end
+
+    n = 10^4
+
+    suite["xf"] = @benchmarkable(
+        foldl(+, ed; simd=true);
+        setup=(ed = multiplied(randn($n), randn($n))))
+    suite["blas"] = @benchmarkable(
+        dot(xs, ys),
+        setup=(xs = randn($n); ys = randn($n)))
+    suite["man"] = @benchmarkable(
+        manual_dot(xs, ys),
+        setup=(xs = randn($n); ys = randn($n)))
+
+    suite
+end
