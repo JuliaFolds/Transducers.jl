a2234f0d64ccb8ce77365aceaeeea63829d4bb15
Takafumi Arakaki <aka.tkf@gmail.com>
2019-01-20 21:25:56 -0800

    Add bench_missing_argmax.jl

---
+++ benchmark/bench_missing_argmax.jl
@@ -0,0 +1,64 @@
+module BenchMissingArgmax
+
+using BenchmarkTools
+using Transducers
+
+function manual_missing_argmax(xs)
+    v = typemin(Base.nonmissingtype(eltype(xs)))
+    n = 0
+    @inbounds @simd for i in eachindex(xs)
+        x = xs[i]
+        if x !== missing
+            if x > v
+                v = x
+                n = i
+            end
+        end
+    end
+    return n, v
+end
+
+random_missings(n = 10^3, th=2) = [abs(x) > th ? missing : x for x in randn(n)]
+
+argext_step(should_update) =
+    ((oldindex, oldvalue), (index, value)) ->
+        should_update(oldvalue, value) ? (index, value) : (oldindex, oldvalue)
+init_helper(::typeof(>), ::Type{Tuple{F, S}}) where {F, S} = (zero(F), typemax(S))
+init_helper(::typeof(<), ::Type{Tuple{F, S}}) where {F, S} = (zero(F), typemin(S))
+argext_init(should_update) = Initializer(TT -> init_helper(should_update, TT))
+
+xf_scanext(should_update) = Scan(argext_step(should_update),
+                                 argext_init(should_update))
+
+xf_argmax = Zip(Count(), NotA(Missing)) |> xf_scanext(<)
+
+suite = BenchmarkGroup()
+
+let xs = randn(10)
+    @assert manual_missing_argmax(xs)[1] == argmax(xs)
+end
+
+let xs = random_missings()
+    @assert manual_missing_argmax(xs) == foldl(right, eduction(xf_argmax, xs))
+end
+
+suite["xf"] = @benchmarkable(
+    foldl(right, ed),
+    setup=(ed = eduction(xf_argmax, random_missings())))
+
+rf = Transducers.reducingfunction(
+    xf_argmax,
+    right,
+    Union{Missing, Float64};
+    # simd = true,
+)
+suite["rf"] = @benchmarkable(
+    transduce($rf, $(argext_init(<)), xs),
+    setup=(xs = random_missings()))
+
+suite["man"] = @benchmarkable(
+    manual_missing_argmax(xs),
+    setup=(xs = random_missings()))
+
+end  # module
+BenchMissingArgmax.suite
