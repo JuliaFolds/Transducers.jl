d33a0f124edd18e1951740bdf1d74a2304454175
Takafumi Arakaki <aka.tkf@gmail.com>
2019-07-13 17:57:37 -0700

    Compare fused multiplications with BLAS

--- benchmark/bench_gemm.jl
+++ benchmark/bench_gemm.jl
@@ -7,9 +7,12 @@ using Referenceables: referenceable
 using Transducers
 using Transducers: @simd_if, @next!, complete, maybe_usesimd, BottomRF, SideEffect
 
+_size(x) = size.(axes(x), 1)
+_size(x, i) = size(axes(x)[i], 1)
+
 @inline function ij_ik_kj_foldable(C, A, B)
-    @argcheck size(C) === (size(A)[1], size(B)[2])
-    @argcheck size(A)[2] === size(B)[1]
+    @argcheck _size(C) === (_size(A, 1), _size(B, 2))
+    @argcheck _size(A, 2) === _size(B, 1)
     @argcheck !any(Base.has_offset_axes, (A, B, C))
     return AdHocFoldable() do rf, acc, _
         Base.@_inline_meta
@@ -88,14 +91,17 @@ end
 
 
 suite = BenchmarkGroup()
-for impl in ["xf", "man"]
-    suite[impl] = BenchmarkGroup()
-    suite[impl]["false"] = BenchmarkGroup()
-    suite[impl]["true"] = BenchmarkGroup()
-    suite[impl]["ivdep"] = BenchmarkGroup()
-end
 
-suite["linalg"] = BenchmarkGroup()
+let sub = suite["mul"] = BenchmarkGroup()
+    for impl in ["xf", "man"]
+        sub[impl] = BenchmarkGroup()
+        sub[impl]["false"] = BenchmarkGroup()
+        sub[impl]["true"] = BenchmarkGroup()
+        sub[impl]["ivdep"] = BenchmarkGroup()
+    end
+
+    sub["linalg"] = BenchmarkGroup()
+end
 
 for n in [8, 32, 256]
     A = randn(n, n)
@@ -103,14 +109,63 @@ for n in [8, 32, 256]
     desired = A * B
     C = zero(desired)
 
+    sub = suite["mul"]
     for (impl, f) in [("xf", xfmul!), ("man", manmul!)]
         for simd in [false, true, :ivdep]
-            suite[impl][string(simd)][n] = @benchmarkable(
+            sub[impl][string(simd)][n] = @benchmarkable(
                 $f($C, $A, $B, $(Val(simd)))
             )
         end
     end
-    suite["linalg"][n] = @benchmarkable mul!($C, $A, $B)
+    sub["linalg"][n] = @benchmarkable mul!($C, $A, $B)
+end
+
+function fusedxfmul!(C, A, B1, B2)
+    fill!(C, 0)
+
+    B = Broadcast.instantiate(Broadcast.broadcasted(tuple, B1, B2))
+    CAB = ij_ik_kj_foldable(referenceable(C), A, B)
+
+    foreach(CAB; simd=Val(:ivdep)) do (c, a, (b1, b2))
+        c0 = c[]
+        c1 = muladd(a, b1, c0)
+        c2 = a^2 * b2
+        c[] = c1 + c2
+    end
+
+    return C
+end
+
+function nonfusedmul!(C, A1, A2, B1, B2)
+    Ⅰ = one(eltype(C))
+    ∅ = zero(eltype(C))
+    A2 .= A1 .^ 2
+    BLAS.gemm!('N', 'N', Ⅰ, A1, B1, ∅, C)
+    BLAS.gemm!('N', 'N', Ⅰ, A2, B2, Ⅰ, C)
+    return C
+end
+
+let sub = suite["fusedmul"] = BenchmarkGroup(),
+    n = 1024
+
+    sub["xf"] = BenchmarkGroup()
+    sub["blas"] = BenchmarkGroup()
+
+    for m in [2, 8, 16, 32]
+        A1 = randn(n, n)
+        A2 = A1 .^ 2
+        B1 = randn(n, m)
+        B2 = randn(n, m)
+        C = zero(A1 * B2)
+
+        @assert isapprox(
+            copy(fusedxfmul!(C, A1, B1, B2)),
+            copy(nonfusedmul!(C, A1, A2, B1, B2)),
+        )
+
+        sub["xf"][m] = @benchmarkable fusedxfmul!($C, $A1, $B1, $B2)
+        sub["blas"][m] = @benchmarkable nonfusedmul!($C, $A1, $A2, $B1, $B2)
+    end
 end
 
 end  # module
