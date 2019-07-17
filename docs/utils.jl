eab67ddb94e0e6c82c0b65c41b1e218287d8675a
Takafumi Arakaki <aka.tkf@gmail.com>
2019-07-16 18:47:24 -0700

    Remove prime sieve example from documentation
    Actually, inner mapfoldl does not make much sense.  I could just
    compose closure that outputs true/false.

--- docs/utils.jl
+++ docs/utils.jl
@@ -8,7 +8,6 @@ EXAMPLE_PAGES = [
     "Tutorial: Missing values" => "examples/tutorial_missings.md",
     "Empty result handling" => "examples/empty_result_handling.md",
     "Parallel word count" => "examples/words.md",
-    "Prime sieve" => "examples/primes.md",
     "Writing transducers" => "examples/transducers.md",
     "Writing reducibles" => "examples/reducibles.md",
 ]
