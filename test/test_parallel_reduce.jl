9d8346776a778df96f7c22003452399f7bb7117f
Takafumi Arakaki <aka.tkf@gmail.com>
2019-08-21 21:58:51 -0700

    Test deterministic termination

--- test/test_parallel_reduce.jl
+++ test/test_parallel_reduce.jl
@@ -1,25 +1,28 @@
 module TestParallelReduce
 include("preamble.jl")
 
-const records = []
-const records_lock = Threads.SpinLock()
+struct Recorder
+    records
+    lock
+end
 
-find(needle) = function(found, x)
+Recorder() = Recorder([], Threads.SpinLock())
+
+function (recorder::Recorder)(x)
     try
-        lock(records_lock)
-        push!(records, x)
+        lock(recorder.lock)
+        push!(recorder.records, x)
+        return x
     finally
-        unlock(records_lock)
-    end
-    if x == needle
-        Reduced(true)
-    else
-        found
+        unlock(recorder.lock)
     end
 end
 
 @testset "early termination (runtime)" begin
-    @test reduce(find(1), Map(identity), 1:32; basesize=1, init=false)
+    recorder = Recorder()
+    records = recorder.records
+    @test reduce(right, Map(recorder) |> ReduceIf(==(1)), 1:32;
+                 basesize=1) === 1
     if VERSION >= v"1.3-alpha"
         if Threads.nthreads() == 1
             @test length(records) == 1
@@ -34,9 +37,8 @@ end
 
 @testset "early termination (grid)" begin
     @testset for needle in 1:20, len in 1:20
-        empty!(records)
-        @test reduce(find(needle), Map(identity), 1:len;
-                     basesize=1, init=false) == (needle <= len)
+        @test reduce(right, ReduceIf(x -> x >= needle), 1:len; basesize=1) ==
+            min(needle, len)
     end
 end
 
