73c90d9e99cb4256c1471e8596710c8fae26a211
Takafumi Arakaki <aka.tkf@gmail.com>
2019-09-14 15:39:54 -0700

    Make benchmarks less verbose in CI

--- benchmark/runjudge.jl
+++ benchmark/runjudge.jl
@@ -9,15 +9,24 @@ mkconfig(; kwargs...) =
         kwargs...
     )
 
+progressoptions =
+    if lowercase(get(ENV, "CI", "false")) == "true"
+        (dt = 60 * 9.0,)
+    else
+        NamedTuple()
+    end
+
 group_target = benchmarkpkg(
     dirname(@__DIR__),
     mkconfig(),
+    progressoptions = progressoptions,
     resultfile = joinpath(@__DIR__, "result-target.json"),
 )
 
 group_baseline = benchmarkpkg(
     dirname(@__DIR__),
     mkconfig(id = "master"),
+    progressoptions = progressoptions,
     resultfile = joinpath(@__DIR__, "result-baseline.json"),
 )
 
