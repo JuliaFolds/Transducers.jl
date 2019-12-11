8333a1a894890d677605fba683b63921386545fd
Takafumi Arakaki <aka.tkf@gmail.com>
2019-12-10 23:23:33 -0800

    Use Run.jl to build documentation

--- docs/make.jl
+++ docs/make.jl
@@ -5,8 +5,7 @@ JULIA="${JULIA:-julia --color=yes --startup-file=no}"
 export JULIA_PROJECT="$(dirname ${BASH_SOURCE[0]})"
 
 set -ex
-${JULIA} -e 'using Pkg; Pkg.instantiate();
-             Pkg.develop(PackageSpec(path=pwd()))'
+${JULIA} -e 'using Pkg; Pkg.instantiate()'
 
 export JULIA_LOAD_PATH="@"
 exec ${JULIA} "${BASH_SOURCE[0]}" "$@"
