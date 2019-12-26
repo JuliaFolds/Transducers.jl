1b0f6adbf2a9223388e0107e4fc2c2f476c3dd16
Takafumi Arakaki <aka.tkf@gmail.com>
2019-12-26 13:50:28 -0800

    Test early termination

--- test/test_unordered.jl
+++ test/test_unordered.jl
@@ -12,9 +12,20 @@ using Transducers: append_unordered!, channel_unordered
 end
 
 @testset "channel_unordered" begin
-    input = Channel(Map(identity), 1:100)
-    ys = sort!(collect(channel_unordered(Map(inc), input)))
-    @test ys == (1:100) .+ 1
+    @testset "Map(inc)" begin
+        input = Channel(Map(identity), 1:100)
+        ys = sort!(collect(channel_unordered(Map(inc), input)))
+        @test ys == (1:100) .+ 1
+    end
+    @testset "ReduceIf(isodd): early termination" begin
+        input_data = fill(2, 100)
+        input_data[10] = 1
+        input = Channel(Map(identity), input_data)
+        ys = collect(channel_unordered(ReduceIf(isodd), input))
+        @test count(x -> x == 1, ys) == 1
+        @test sort!(unique(ys)) == [1, 2]
+        @test length(ys) < length(input_data)
+    end
 end
 
 end  # module
