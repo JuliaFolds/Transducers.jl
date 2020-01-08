0aee352ccb92fbcebdae93c89c56378cb2a5bedd
Takafumi Arakaki <aka.tkf@gmail.com>
2020-01-07 19:13:59 -0800

    Fix `dtransduce` with `withprogress`

--- src/progress.jl
+++ src/progress.jl
@@ -132,7 +132,7 @@ start(rf::R_{LogProgressOnCombine}, result) =
     return wrap(rf, (tc, nc), irc)
 end
 
-function _reduce_progress(reduce_impl, rf0, init, coll)
+function setup_logprogressoncombine(rf0, interval, chan)
     if rf0 isa R_{UseSIMD}
         xf0 = xform(rf0)
         rfinner = inner(rf0)
@@ -141,10 +141,14 @@ function _reduce_progress(reduce_impl, rf0, init, coll)
         rfinner = rf0
     end
 
-    chan = Channel{Int}(0)
-    xf = xf0 |> LogProgressOnCombine(chan, coll.reducible.interval)
+    xf = xf0 |> LogProgressOnCombine(chan, interval)
     rf = Reduction(xf, rfinner)
+    return rf
+end
 
+function _reduce_progress(reduce_impl, rf0, init, coll)
+    chan = Channel{Int}(0)
+    rf = setup_logprogressoncombine(rf0, coll.reducible.interval, chan)
     reducible = @set coll.reducible = coll.reducible.foldable
     progress_task = @async let n = length(coll.reducible.foldable)
         __progress() do id
@@ -192,28 +196,21 @@ end
 # https://github.com/timholy/Revise.jl/pull/157
 
 function (foldl::RemoteReduceWithLogging)(rf0, init, coll, basesize)
-    xf = ScanEmit((0, time())) do (n, t0), x
-        t1 = time()
-        n += 1
-        if t1 - t0 > foldl.progress_interval
-            put!(chan, n)
-            n = 0
-        end
-        x, (n, t1)
-    end
-    rf = Reduction(xf, rf0)
     chan = _Channel(eltype(foldl.chan), 0) do chan
         while true
             put!(foldl.chan, take!(chan))
         end
     end
+    local rf
     result = try
+        rf = setup_logprogressoncombine(rf0, foldl.progress_interval, chan)
         _transduce_assoc_nocomplete(rf, init, coll, basesize)
     finally
         close(chan)
     end
     result isa Reduced && return result
-    _, iresult = unwrap(rf, result)  # manually unwrap ScanEmit's private state
+    # Manually unwrap LogProgressOnCombine's private state:
+    _, iresult = unwrap(rf, result)
     return iresult
 end
 
