597ffe7dfd0636383b0f8b8db79e24b954aa4b44
Takafumi Arakaki <aka.tkf@gmail.com>
2020-01-21 18:54:31 -0800

    Add multi-thread/bench_sum.jl

---
+++ benchmark/multi-thread/bench_sum.jl
@@ -0,0 +1,56 @@
+module BenchSum
+
+using BenchmarkTools
+using Transducers
+
+suite = BenchmarkGroup()
+
+function slow_computation(n)
+    x = 0.4
+    for _ in 1:n
+        x = 3 / π * sin(x * π)
+    end
+    return x
+end
+
+xf = Map(slow_computation)
+
+n = 2^18
+inputtable = [
+    ("uniform", fill(500, n)),
+    ("random", rand(1:1000, n)),
+    ("increasing", ceil.(Int, range(1, 1000, length = n))),
+    ("decreasing", ceil.(Int, range(1000, 1, length = n))),
+    (
+        "mountain",
+        vcat(
+            ceil.(Int, range(1, 1000, length = n ÷ 2)),
+            ceil.(Int, range(1000, 1, length = n ÷ 2)),
+        ),
+    ),
+    (
+        "valley",
+        vcat(
+            ceil.(Int, range(1000, 1, length = n ÷ 2)),
+            ceil.(Int, range(1, 1000, length = n ÷ 2)),
+        ),
+    ),
+]
+
+for (label, xs) in inputtable
+    s1 = suite[label] = BenchmarkGroup()
+    s1["foldl"] = @benchmarkable foldl(+, $xf, $xs; simd = true)
+    s2 = s1["reduce"] = BenchmarkGroup()
+    for basesize in 2 .^ (7:9)
+        s2["basesize=$basesize"] = @benchmarkable reduce(
+            +,
+            $xf,
+            $xs;
+            basesize = $basesize,
+            simd = true,
+        )
+    end
+end
+
+end  # module
+BenchSum.suite
