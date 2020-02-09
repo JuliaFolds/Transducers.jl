172a462115539530c71c723e8fc653d4313952bb
Takafumi Arakaki <aka.tkf@gmail.com>
2020-02-08 20:53:25 -0800

    Use PerformanceTestTools

--- test/test_ir.jl
+++ test/test_ir.jl
@@ -1,25 +1,6 @@
 module TestIR
 
-using Test
-
-if Base.JLOptions().check_bounds == 1 || Base.JLOptions().code_coverage != 0
-    # Run IR tests in a different process as `--check-bounds=yes` or
-    # --code-coverage={user|all} is specified.
-    path = joinpath(@__DIR__, "__test_ir.jl")
-    code = """
-    $(Base.load_path_setup_code())
-    include($(repr(path)))
-    """
-    cmd = Base.julia_cmd()
-    cmd = `$cmd --check-bounds=no --code-coverage=none`
-    if Base.JLOptions().color == 1
-        cmd = `$cmd --color=yes`
-    end
-    @info "Running IR test in a subprocess..." cmd
-    @test success(pipeline(`$cmd -e $code`; stdout=stdout, stderr=stderr))
-    @info "Running IR test in a subprocess...DONE"
-else
-    include("__test_ir.jl")
-end
+import PerformanceTestTools
+PerformanceTestTools.@include("__test_ir.jl")
 
 end  # module
