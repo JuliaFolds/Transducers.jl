e0752bb6d6a6d0643b392148fed5d44f6e18ee83
Takafumi Arakaki <aka.tkf@gmail.com>
2020-03-12 07:22:52 -0700

    Fix overhead benchmark with terminatable=true

--- benchmark/multi-thread/bench_overhead.jl
+++ benchmark/multi-thread/bench_overhead.jl
@@ -8,11 +8,19 @@ suite = BenchmarkGroup()
 let n = 64
     # s = suite["n=$n"] = BenchmarkGroup()
     s = suite  # there is only one `n` for now
+    s["default"] = @benchmarkable reduce(
+        right,
+        Map(_ -> nothing),
+        1:$n;
+        basesize = 1,
+        # terminatable = nothing,
+    )
     s["terminatable=true"] = @benchmarkable reduce(
         right,
         Map(_ -> nothing),
         1:$n;
         basesize = 1,
+        terminatable = true,
     )
     s["terminatable=false"] = @benchmarkable reduce(
         right,
