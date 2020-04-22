b50a28ed66d97bc6f1dfab5ae7541058508ab418
Takafumi Arakaki <aka.tkf@gmail.com>
2020-04-21 22:04:52 -0700

    Workaround: at-testset is not return-safe

--- test/test_doctest.jl
+++ test/test_doctest.jl
@@ -7,14 +7,13 @@ using Transducers
 @testset "doctest" begin
     if lowercase(get(ENV, "JULIA_PKGEVAL", "false")) == "true"
         @info "Skipping doctests on PkgEval."
-        return
     elseif VERSION >= v"1.5-"
         @info "Skipping doctests on Julia $VERSION."
-        return
+    else
+        include("../docs/utils.jl")
+        transducers_rm_examples()
+        doctest(Transducers; manual=true)
     end
-    include("../docs/utils.jl")
-    transducers_rm_examples()
-    doctest(Transducers; manual=true)
 end
 
 end  # module
