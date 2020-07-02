3dfed970f9f3f353afce5190e1d0d3edc05f4c36
Takafumi Arakaki <aka.tkf@gmail.com>
2020-07-01 19:15:09 -0700

    Tweak benchmark labels

--- benchmark/bench_append!!.jl
+++ benchmark/bench_append!!.jl
@@ -1,4 +1,4 @@
-module BenchAppend
+module BenchAppendBB
 
 using BangBang: append!!
 using Transducers
@@ -6,14 +6,14 @@ using BenchmarkTools: @benchmarkable, BenchmarkGroup
 
 const SUITE = BenchmarkGroup()
 
-SUITE["append!!(::Transducer, Union{}[], _)"] = @benchmarkable(
+SUITE["xf"] = @benchmarkable(
     append!!(Transducer(y for x in nothing if isodd(x) for y in 1:x), dest, 1:100),
     setup = (dest = Union{}[]),
 )
-SUITE["append!!(Union{}[], ::Eduction)"] = @benchmarkable(
+SUITE["eduction"] = @benchmarkable(
     append!!(dest, eduction(y for x in 1:100 if isodd(x) for y in 1:x)),
     setup = (dest = Union{}[]),
 )
 
 end  # module
-BenchAppend.SUITE
+BenchAppendBB.SUITE
