JuliaFolds/Transducers.jl

commit eb430cf76a5c25aa909858c20424aead21cfecfd

Tail-call function-barrier for arrays (#403)
This PR implements the "tail-call function-barrier" pattern for arrays
with linear style indexing.  This gives us a better performance for
type-changing reduction where the iteration at which the type changes
is unknown.  A good example is filtered sum with no initial value
(#404).

Interestingly, this version is a bit worse for sum of filtered floats
on "short" arrays.  Compare the result of
`["filter_sum", "1000", "RandomFloats", "noinit", "xf"]`.  In my
laptop, baseline is ~1.7 μs while this branch is ~2.1 μs.  However,
for 10x longer input, this branch is faster (> 2x).  The benchmarks on
CI also show similar results.

src/basics.jl
@@ -95,3 +95,26 @@ abstract type _Function <: Function end
 Base.show(io::IO, ::MIME"text/plain", f::_Function) = show(io, f)
 Base.print(io::IO, f::_Function) = show(io, f)
 @specialize
+
+
+# A macro for "manual Union splitting".  It is sometimes useful to let
+# the compiler know that it is beneficial to type-specialize `body`.
+# * https://github.com/JuliaFolds/Transducers.jl/pull/188
+# * https://github.com/JuliaLang/julia/pull/34293#discussion_r363550608
+macro manual_union_split(cond, body)
+    quote
+        if $cond
+            $body
+        else
+            $body
+        end
+    end |> esc
+end
+
+@inline _firstindex(arr) = firstindex(arr)
+@inline _lastindex(arr) = lastindex(arr)
+
+# Define `firstindex` and `lastindex` for `Broadcasted` with linear
+# index style:
+@inline _firstindex(bc::Broadcasted) = first((axes(bc)::Tuple{Any})[1])
+@inline _lastindex(bc::Broadcasted) = last((axes(bc)::Tuple{Any})[1])

src/processes.jl
@@ -155,15 +155,7 @@ function __foldl__(rf::RF, init::T, coll) where {RF,T}
     ret === nothing && return complete(rf, init)
     x, state = ret
     val = @next(rf, init, x)
-
-    # Doing "manual Union splitting" (?).  This somehow helps the
-    # compiler to generate faster code even though the code inside the
-    # `if` branches are identical.
-    # * https://github.com/JuliaFolds/Transducers.jl/pull/188
-    # * https://github.com/JuliaLang/julia/pull/34293#discussion_r363550608
-    if val isa T
-        return _foldl_iter(rf, val, coll, state, FOLDL_RECURSION_LIMIT)
-    else
+    @manual_union_split val isa T begin
         return _foldl_iter(rf, val, coll, state, FOLDL_RECURSION_LIMIT)
     end
 end
@@ -185,15 +177,46 @@ end
     complete(rf, @return_if_reduced foldlargs(rf, init, coll...))
 
 # TODO: use IndexStyle
-@inline function __foldl__(rf::RF, init, arr::Union{AbstractArray,Broadcasted}) where {RF}
+@inline function __foldl__(
+    rf::RF,
+    init::T,
+    arr::Union{AbstractArray,Broadcasted},
+) where {RF,T}
     isempty(arr) && return complete(rf, init)
-    idxs = eachindex(arr)
-    val = @next(rf, init, @inbounds arr[idxs[firstindex(idxs)]])
-    @simd_if rf for k in firstindex(idxs) + 1:lastindex(idxs)
-        i = @inbounds idxs[k]
-        val = @next(rf, val, @inbounds arr[i])
+    i = _firstindex(arr)
+    acc = @next(rf, init, @inbounds arr[i])
+    @manual_union_split acc isa T begin
+        if is_prelude(acc)
+            return _foldl_linear_rec(rf, acc, arr, i + 1, FOLDL_RECURSION_LIMIT)
+        else
+            return _foldl_linear_bulk(rf, acc, arr, i + 1)
+        end
     end
-    return complete(rf, val)
+end
+
+@inline function _foldl_linear_bulk(rf::RF, acc, arr, i0) where {RF}
+    @simd_if rf for i in i0:_lastindex(arr)
+        acc = @next(rf, acc, @inbounds arr[i])
+    end
+    return complete(rf, acc)
+end
+
+@inline function _foldl_linear_rec(rf::RF, acc::T, arr, i0, counter) where {RF,T}
+    for i in i0:_lastindex(arr)
+        y = @next(rf, acc, @inbounds arr[i])
+        if counter !== Val(0)
+            if y isa T
+            elseif is_prelude(y)
+                return _foldl_linear_rec(rf, y, arr, i + 1, _dec(counter))
+            else
+                # Otherwise, maybe it could be something like `Union{Float64,Missing}`
+                # where Julia's native loop is fast:
+                return _foldl_linear_bulk(rf, y, arr, i + 1)
+            end
+        end
+        acc = y
+    end
+    return complete(rf, acc)
 end
 
 @inline _getvalues(i) = ()

test/test_processes.jl
@@ -47,6 +47,15 @@ include("preamble.jl")
         @test_throws EmptyResultError foldl(+, nested_xf, iter)
     end
 
+    @testset "type-unstable arrays" begin
+        valof(::Val{x}) where {x} = x
+        valof(x) = x
+        @testset for n in 1:valof(Transducers.FOLDL_RECURSION_LIMIT) + 3
+            @test collect(Map(valof), [Val(i) for i in 1:n]) == 1:n
+        end
+        @test collect(Map(valof), [[Val(i) for i in 1:4]; 5:9;]) == 1:9
+    end
+
     @testset "zip-of-arrays" begin
         @testset for arrays in [
                 (0:3,),

src/core.jl
@@ -565,6 +565,9 @@ combine(rf::Reduction, a, b) =
         combine(inner(rf), a, b)
     end
 
+is_prelude(_) = false
+is_prelude(::InitialValues.InitialValue) = true
+
 privatestate(::T, state, result) where {T <: AbstractReduction} =
     privatestate(T, state, result)
 
@@ -597,6 +600,8 @@ ownsstate(::R, ::PrivateState{T}) where {R, T} = R === T
 # compile Extrema examples in ../examples/tutorial_missings.jl (it
 # took more than 10 min).  See also:
 # https://github.com/JuliaLang/julia/issues/30125
+
+@inline is_prelude(ps::PrivateState) = is_prelude(psstate(ps)) || is_prelude(psresult(ps))
 
 """
     unwrap(rf, result)

src/library.jl
@@ -927,6 +927,7 @@ struct PartitionBy{F} <: Transducer
 end
 
 struct Unseen end
+is_prelude(::Unseen) = true
 
 isexpansive(::PartitionBy) = false
 

commit 735aae6d97e2671562034d78af66eba6b9933e60

Specialize foldl for cartesian style arrays (#407)
This patch implements a specialization of `foldl` on arrays with
`IndexCartesian` index style.  This gives us more than 2x speedup (see
`sum_transpose` benchmark).

The implementation mostly just redirects the call to `foldl` of
`CartesianIndices`.  Most of the code is for compatibility with Julia
< 1.5.  This PR also fixes a bug in `foldl` for multi-dimensional
`Broadcasted` (probably introduced by #403).

src/basics.jl
@@ -118,3 +118,13 @@ end
 # index style:
 @inline _firstindex(bc::Broadcasted) = first((axes(bc)::Tuple{Any})[1])
 @inline _lastindex(bc::Broadcasted) = last((axes(bc)::Tuple{Any})[1])
+
+# Define `CartesianIndices` for `Broadcasted`
+@inline _CartesianIndices(arr) = CartesianIndices(arr)
+@inline _CartesianIndices(bc::Broadcasted) = CartesianIndices(axes(bc)::Tuple)
+
+# Define `IndexStyle` for `Broadcasted`
+_IndexStyle(arr) = IndexStyle(arr)
+_IndexStyle(bc::Broadcasted) = _IndexStyle(typeof(bc))
+_IndexStyle(::Type{<:Broadcasted{<:Any,<:Tuple{Any}}}) = IndexLinear()
+_IndexStyle(::Type{<:Broadcasted{<:Any}}) = IndexCartesian()

src/processes.jl
@@ -176,13 +176,13 @@ end
 @inline __foldl__(rf::RF, init, coll::Tuple) where {RF} =
     complete(rf, @return_if_reduced foldlargs(rf, init, coll...))
 
-# TODO: use IndexStyle
-@inline function __foldl__(
-    rf::RF,
-    init::T,
-    arr::Union{AbstractArray,Broadcasted},
-) where {RF,T}
+@inline function __foldl__(rf::RF, init, arr0::Union{AbstractArray,Broadcasted}) where {RF}
+    arr = Broadcast.instantiate(arr0)
     isempty(arr) && return complete(rf, init)
+    return _foldl_array(rf, init, arr, _IndexStyle(arr))
+end
+
+@inline function _foldl_array(rf::RF, init::T, arr, ::IndexLinear) where {RF,T}
     i = _firstindex(arr)
     acc = @next(rf, init, @inbounds arr[i])
     @manual_union_split acc isa T begin
@@ -217,6 +217,12 @@ end
         acc = y
     end
     return complete(rf, acc)
+end
+
+@inline function _foldl_array(rf0::RF, init, arr, ::IndexStyle) where {RF,T}
+    @inline getvalue(I) = @inbounds arr[I]
+    rf = Map(getvalue)'(rf0)
+    return __foldl__(rf, init, _CartesianIndices(arr))
 end
 
 @inline _getvalues(i) = ()

test/test_processes.jl
@@ -110,13 +110,20 @@ include("preamble.jl")
         end
     end
 
-    @testset "broadcast" begin
+    @testset "broadcast (linear)" begin
         @testset for xs in iterator_variants(1:3)
             ys = @~ xs.^2
             @test collect(Map(identity), ys) == copy(ys)
             @test foldl(+, Filter(isodd), ys) == 10
             @test foldl(+, Filter(isodd), ys; init=0) == 10
         end
+    end
+
+    @testset "broadcast (cartesian)" begin
+        xs = @~ (1:3) .+ (4:5)'
+        @test collect(Map(identity), xs) == vec(copy(xs))
+        @test foldl(+, Filter(isodd), xs) == 19
+        @test foldl(+, Filter(isodd), xs; init = 0) == 19
     end
 end
 