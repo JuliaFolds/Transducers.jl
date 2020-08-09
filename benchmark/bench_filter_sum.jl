b1b48c2b922cbc37ca496b0cef8cb159b60b8a25
Takafumi Arakaki <aka.tkf@gmail.com>
2020-08-08 21:27:08 -0700

    Try different sizes

--- benchmark/bench_filter_sum.jl
+++ benchmark/bench_filter_sum.jl
@@ -5,7 +5,6 @@ using BenchmarkTools
 using Transducers
 
 const SUITE = BenchmarkGroup()
-const N = 1000
 
 function naive_sum(xs, acc = false)
     for x in xs
@@ -15,21 +14,25 @@ function naive_sum(xs, acc = false)
 end
 
 Random.seed!(12345)
-for (xslabel, xs, init) in [
-    (:UnitRange, (x for x in 1:N if isodd(x)), 0),
-    (:RandomFloats, (x for x in randn(N) if x > 0), 0.0),
-]
-    s1 = SUITE[:xs=>xslabel] = BenchmarkGroup()
+for n in [1000, 10000]
+    s0 = SUITE[:n=>n] = BenchmarkGroup()
 
-    s2 = s1[:withinit=>false] = BenchmarkGroup()
-    s2[:impl=>:naive] = @benchmarkable naive_sum($xs)
-    s2[:impl=>:base] = @benchmarkable sum($xs)
-    s2[:impl=>:xf] = @benchmarkable sum($(eduction(xs)))
+    for (xslabel, xs, init) in [
+        (:UnitRange, (x for x in 1:n if isodd(x)), 0),
+        (:RandomFloats, (x for x in randn(n) if x > 0), 0.0),
+    ]
+        s1 = s0[:xs=>xslabel] = BenchmarkGroup()
 
-    s2 = s1[:withinit=>true] = BenchmarkGroup()
-    s2[:impl=>:naive] = @benchmarkable naive_sum($xs, $init)
-    s2[:impl=>:base] = @benchmarkable foldl(+, $xs; init = $init)
-    s2[:impl=>:xf] = @benchmarkable foldl(+, $(eduction(xs)); init = $init)
+        s2 = s1[:withinit=>false] = BenchmarkGroup()
+        s2[:impl=>:naive] = @benchmarkable naive_sum($xs)
+        s2[:impl=>:base] = @benchmarkable sum($xs)
+        s2[:impl=>:xf] = @benchmarkable sum($(eduction(xs)))
+
+        s2 = s1[:withinit=>true] = BenchmarkGroup()
+        s2[:impl=>:naive] = @benchmarkable naive_sum($xs, $init)
+        s2[:impl=>:base] = @benchmarkable foldl(+, $xs; init = $init)
+        s2[:impl=>:xf] = @benchmarkable foldl(+, $(eduction(xs)); init = $init)
+    end
 end
 
 end  # module
