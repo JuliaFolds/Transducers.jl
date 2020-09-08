7e0cc728280a801a03c348e475945cbd7bc120f8
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-07 18:23:06 -0700

    Simplify test/test_aqua.jl and check Project.toml formatting (#423)
    Use flag-based configuration added in Aqua 0.4.7.

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -5,47 +5,20 @@ using Setfield
 using Test
 using Transducers
 
-# Aqua.test_all(  # TODO: make it work
-#     Transducers;
-#     # https://github.com/JuliaCollections/DataStructures.jl/pull/511
-#     ambiguities=(exclude=[Base.get, Setfield.set, Setfield.modify],),
-# )
+Aqua.test_all(
+    Transducers;
+    # https://github.com/JuliaCollections/DataStructures.jl/pull/511
+    ambiguities = (; exclude = [Base.get, Setfield.set, Setfield.modify, map!]),
+    unbound_args = false,  # TODO: make it work
+    project_extras = true,
+    stale_deps = true,
+    deps_compat = true,
+    project_toml_formatting = true,
+)
 
-@testset "Method ambiguity" begin
-    if VERSION >= v"1.6.0-DEV.816"
-        @warn "Ignoring ambiguities from `Base` to workaround JuliaLang/julia#36962"
-        packages = [Transducers]
-    else
-        packages = [Transducers, Base]
-    end
-    Aqua.test_ambiguities(
-        packages,
-        exclude = [Base.get, Setfield.set, Setfield.modify, map!],
-    )
-end
-
-# @testset "Unbound type parameters" begin
-#     Aqua.test_unbound_args(Transducers)
-# end
-
-@testset "Undefined exports" begin
-    Aqua.test_undefined_exports(Transducers)
-end
-
-@testset "Compare Project.toml and test/Project.toml" begin
-    Aqua.test_project_extras(Transducers)
-    @testset "... and test/environments/main/Project.toml" begin
-        @test Text(read(joinpath(@__DIR__, "Project.toml"), String)) ==
-            Text(read(joinpath(@__DIR__, "environments", "main", "Project.toml"), String))
-    end
-end
-
-@testset "Stale dependencies" begin
-    Aqua.test_stale_deps(Transducers)
-end
-
-@testset "Compat bounds" begin
-    Aqua.test_deps_compat(Transducers)
+@testset "Compare test/Project.toml and test/environments/main/Project.toml" begin
+    @test Text(read(joinpath(@__DIR__, "Project.toml"), String)) ==
+          Text(read(joinpath(@__DIR__, "environments", "main", "Project.toml"), String))
 end
 
 end  # module
