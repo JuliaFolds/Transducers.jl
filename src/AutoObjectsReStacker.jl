ea0a4b4f1133dc6441d3df839eb6fa54fdacb4d3
maarten <maarten.vandamme@ugent.be>
2021-10-16 09:07:04 +0200

    simlified version

--- src/AutoObjectsReStacker.jl
+++ src/AutoObjectsReStacker.jl
@@ -29,33 +29,12 @@ _getnames(::Type{<:NamedTuple{names}}) where names = names
         ex = :x
     elseif isstructtype(x)
         fn = fieldnames(x);
-
-        cfield = QuoteNode(fn[1]);
-        new = Expr(:new,x,:(restack(getfield(x,$cfield))));
+        cfield = QuoteNode(fn[end]);
+        new = Expr(:new,x,map(n -> :(restack(getfield(x, $(QuoteNode(n))))), fn)...);
 
         ex = quote
-            if isdefined(x,$(cfield))
+            if isimmutable(x) && isdefined(x,$(cfield))
                 $(new)
-            else
-                x
-            end
-        end
-
-        for i in 2:length(fn)
-            cfield = QuoteNode(fn[i]);
-            new = Expr(:new,x,map(n -> :(restack(getfield(x, $(QuoteNode(n))))), fn[1:i])...);
-            ex = quote
-                if isdefined(x,$(cfield))
-                    $(new)
-                else
-                    $(ex)
-                end
-            end
-        end
-
-        ex = quote
-            if isimmutable(x)
-                $(ex)
             else
                 x
             end
