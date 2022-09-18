cfa2af0fa8e381046d265fd33b04c34a4189312a
Mason Protter <mason.protter@icloud.com>
2022-09-18 16:58:59 -0600

    add docstring for `asfoldable`

--- src/core.jl
+++ src/core.jl
@@ -884,6 +884,26 @@ identityof(::typeof(right), ::Any) = nothing
 
 abstract type Reducible end
 abstract type Foldable <: Reducible end
+
+"""
+   Transducers.asfoldable(x) -> foldable
+   
+By default, this function does nothing, but it can be overloaded to convert an input into another type before reducing over it.
+This allows one to implement a foldable in terms of transducers over an existing type. For instance,
+
+```julia
+struct VectorOfVectors{T}
+   v::Vector{Vector{T}}
+end
+
+Transducers.asfoldable(vov::VectorOfVectors{T}) = vov.v |> Cat()
+```
+Now we can do things like
+```julia
+julia> foldxl(+, VectorOfVectors([[1,2], [3, 4]]))
+10
+```
+"""
 asfoldable(x) = x
 
 """
