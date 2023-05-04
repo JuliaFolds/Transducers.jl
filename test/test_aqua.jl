6518a6caff8a8f7398a687171ff30dfdcb7cae5c
Mason Protter <mason.protter@gmail.com>
2023-05-04 15:24:20 -0600

    skip ambigutites relating to `mapreduce` and `kwcall`

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -8,7 +8,10 @@ using Transducers
 Aqua.test_all(
     Transducers;
     # https://github.com/JuliaCollections/DataStructures.jl/pull/511
-    ambiguities = (; exclude = [Base.get, Setfield.set, Setfield.modify, map!]),
+    ambiguities = (; exclude = [Base.get, Setfield.set, Setfield.modify, map!,
+                                # These are needed because of a potential ambiguity with ChainRulesCore's mapfoldl on a Thunk.
+                                # mapreduce is deprecated, so if we remove it, then that would solve this
+                                mapreduce, Core.kwcall]),
     unbound_args = false,  # TODO: make it work
 )
 
